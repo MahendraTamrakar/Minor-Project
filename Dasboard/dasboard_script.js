544da0d4a5e30334c8a1491e301d0de097cac2e1
MahendraTamrakar <tamrakarmahendra157@gmail.com>
2024-10-05 19:35:57 +0530

    init

---
+++ Dasboard/dasboard_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.menu-button').click(function(){
+        $('.sidebar').addClass('active');
+    });
+
+    $('.close-button').click(function(){
+        $('.sidebar').addClass('active');
+    });
+});
